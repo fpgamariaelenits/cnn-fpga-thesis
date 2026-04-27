e1e80f5024c5e46dbb40d597ace4a02c0ddd8b69
fpgamariaelenits <148952860+fpgamariaelenits@users.noreply.github.com>
2026-04-28 00:17:12 +0300

    Update conv2d_board.tcl

--- vivado/project/conv2d_board.tcl
+++ vivado/project/conv2d_board.tcl
@@ -178,8 +178,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set IP repository paths
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/../../temp/conv2d_filters_1/conv2d_blck/conv2d_blck_ip"]" $obj
-
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/../ip_repo"]" $obj
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild
 }
